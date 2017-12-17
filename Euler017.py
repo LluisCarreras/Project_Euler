4c399c56aa18eb47f844d2721270f5e35065c4c0
Lluis Carreras <lluissot@gmail.com>
2017-12-17 15:10:50 +0100

    Update Euler017.py

--- Euler017.py
+++ Euler017.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan  8 22:01:37 2016
+Created on Sun Dec  17 14:01:37 2017
 
 @author: Lluís Carreras González
 
@@ -20,7 +20,44 @@ forty-two) contains 23 letters and 115 (one hundred and fifteen) contains
 British usage.
 """
 
-units = ["one", "two", "three", "four", "five", "six", "seven", "eight", 
-         "nine", "ten"]
-tens = []
-hundreds 
+
+units_list = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", 
+         "nine"]
+teenagers_list = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", 
+                    "seventeen", "eighteen", "nineteen"]
+tenths_list = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+
+number_letter_counts = {}
+
+for number in range(1, 1001):
+    
+    if number < 10:
+        number_letter_counts[number] = units_list[number]
+        
+    elif number < 20:
+        number_letter_counts[number] = teenagers_list[number - 10]
+        
+    elif number < 100:
+        tenths = number // 10
+        units = number % 10
+        number_letter_counts[number] = tenths_list[tenths] + units_list[units]
+        
+    elif number < 1000:
+        hundreds = number // 100
+        tenths = (number // 10) % 10
+        units = number % 10
+        number_in_letters = units_list[hundreds] + "hundred"
+        if tenths or units:
+            number_in_letters += "and" + number_letter_counts[10 * tenths + units]
+        number_letter_counts[number] = number_in_letters
+        
+    elif number == 1000:
+        number_letter_counts[1000] = "onethousand"
+ 
+       
+total_count = sum([len(value) for key, value in number_letter_counts.items()])
+print(total_count)
+        
+#print(len(number_letter_counts))
+#print(number_letter_counts)
