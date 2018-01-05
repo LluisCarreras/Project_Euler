50210b124885293b2a5a76b05454ed0f248456c0
Lluis Carreras <lluissot@gmail.com>
2018-01-05 12:45:47 +0000

    Problem 15: Lattice paths
    Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
    
    How many such routes are there through a 20×20 grid?

--- Euler015c.py
+++ Euler015c.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Feb  4 16:43:36 2016
-
 @author: Lluís Carreras González
 
 Lattice paths
@@ -10,10 +9,8 @@ Problem 15
 Starting in the top left corner of a 2×2 grid, and only being able to move 
 to the right and down, there are exactly 6 routes to the bottom right corner.
 
-
 How many such routes are there through a 20×20 grid?
 """
-
 
 import time
 
